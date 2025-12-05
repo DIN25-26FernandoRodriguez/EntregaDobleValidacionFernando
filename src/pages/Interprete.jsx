92090f00bb3a81c1a666b162be860147e6e8d696
Fernando <Fernan1590@gmail.com>
2025-12-05 16:27:42 +0100

    Filtrado interpretes hecho

--- src/pages/Interprete.jsx
+++ src/pages/Interprete.jsx
@@ -1,9 +1,28 @@
 import peliculas from "../data/peliculas"
 import Card from "../components/Card.jsx"
+import { useState } from "react";
+import SearchBar from "../components/SearchBar.jsx";
+import { useMemo } from "react";
 
 function Interprete() {
+  
 
 const todosLosActores = peliculas.flatMap(p => p.actores);
+
+const [searchTerm, setSearchTerm] = useState(""); // Declaro el estádo que se va a guardar
+
+  const filteredActores = useMemo(() => {
+    if (!searchTerm) {
+      return todosLosActores;
+      // Si no hay término, devuelve la lista completa
+    }
+    const lowerCaseSearchTerm = searchTerm.toLowerCase();
+    return todosLosActores.filter((actor) =>
+      // Filtra por el nombre de la película
+      actor.nombre.toLowerCase().includes(lowerCaseSearchTerm)
+    );
+  }, [searchTerm]);
+
 
   return (
     <>
@@ -12,12 +31,17 @@ const todosLosActores = peliculas.flatMap(p => p.actores);
           <a href="/inicio" className="text-black-500 hover:underline mb-4 inline-bloc bg-[var(--colorNavBar)] p-2 rounded-2xl ">Volver al inicio</a>
         </button>
 
+        <SearchBar
+        searchTerm={searchTerm}
+        onSearchChange={setSearchTerm}
+        placeholder="Buscar actores por nombre.." />
+
       <div>
         <h1 className="text-4xl font-bold text-center mt-10 mb-6">Intérpretes Destacados</h1>
       </div>
 
       <div className="w-full mx-auto px-4 flex flex-wrap gap-6 justify-center">
-      {
+      {/* {
         todosLosActores.map((actor, index) =>
           <Card
             key={index}
@@ -26,6 +50,20 @@ const todosLosActores = peliculas.flatMap(p => p.actores);
             to={`/interpretes/${index}`} >
             {actor.biografia}
           </Card>
+        )} */}
+        {filteredActores.length > 0 ? (
+          filteredActores.map((actor, index) => (
+            <Card
+            key={index}
+            nombre={actor.nombre}
+            foto={actor.imagen}
+            to={`/interpretes/${index}`} >
+            {actor.biografia}
+          </Card>
+          ))
+        ) : (
+        // Mensaje si no hay resultados
+        <p className="col-span-full text-center text-gray-500 p-4"> No se encontraron películas con el término `{searchTerm}`. </p>
         )}
         </div>
     </>
